81438391338b3700ca65235878f1582298787581
robertramosastudillo <robertramosastudillo@gmail.com>
2020-08-10 20:59:50 -0500

    Inicio de proyecto

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
+
+function App() {
+  // Definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState("");
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=ec4e78482e88489b841870ca326135dc`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
+
+  return (
+    <Fragment>
+      <Header titulo="Buscador de Noticias" />
+      <div className="container white">
+        <Formulario guardarCategoria={guardarCategoria} />
+        <ListadoNoticias noticias={noticias} />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
